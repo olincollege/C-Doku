c2cfc4fdf7fdd3eb9b0f5c5638c24507e135de31
Olga Pidruchna <olgapidruchna@gmail.com>
2023-02-23 22:11:20 -0500

    Implement check_player_board and split check_move into multiple funcs

--- src/model.c
+++ src/model.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
-void update_board(int (*p_board)[9][9], int row, int col, int num) {
-  *p_board[row][col] = num;
+void update_board(int p_board[9][9], int row, int col, int num) {
+  p_board[row][col] = num;
 }
 
-int check_move(int (*com_board)[9][9], int (*p_board)[9][9], int row, int col,
+int move_valid(int com_board[9][9], int p_board[9][9], int row, int col,
                int num) {
   /*
   / int check = check_move(&answer, &player, 3, 3, 1);
     switch(check) {
-      case 0 :
-         printf("Correct Move." );
-         break;
-      case 1 :
-         printf("Incorrect Move." );
-         break;
       case 2 :
          printf("Invalid index. Input a row and column between 1-9.\n" );
          break;
@@ -27,35 +21,34 @@ int check_move(int (*com_board)[9][9], int (*p_board)[9][9], int row, int col,
       default :
          printf("Code error :(" );
   */
-  // Fix h file
   // 2 = invalid index input
   // 3 = invalid number input
   // 4 = filled spot
-  // 0 = correct move
-  // 1 = incorrect move
-  row--;
-  col--;
   if (row < 0 || row > 8 || col < 0 || col > 8) {
     return 2;
   }
   if (num < 1 || num > 9) {
     return 3;
   }
-  if (*p_board[row][col] != 0) {
+  if (p_board[row][col] != 0) {
     return 4;
-  } else {
-    if (*com_board[row][col] == num) {
-      return 0;
-    } else {
-      return 1;
-    }
   }
 }
 
-void board_complete(int (*p_board)[9][9], int *filled) {
+int move_correct(int com_board[9][9], int p_board[9][9], int row, int col,
+                 int num) {
+  // 0 = correct move
+  // 1 = incorrect move
+  if (com_board[row][col] == num) {
+    return 0;
+  }
+  return 1;
+}
+
+void board_complete(int p_board[9][9], int *filled) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      if (*p_board[i][j] == 0) {
+      if (p_board[i][j] == 0) {
         *filled = 0;
         return;
       }
@@ -64,8 +57,16 @@ void board_complete(int (*p_board)[9][9], int *filled) {
   *filled = 1;
 }
 
-int check_player_board(int (*board)[9][9]) {
+int check_player_board(int p_board[9][9], int com_board[9][9]) {
   // checks whether player board is filled in correctly compared to solution
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (p_board[i][j] != com_board[i][j]) {
+        return 1;
+      }
+    }
+  }
+  return 0;
 }
 
 int check_solution_board(int) {
