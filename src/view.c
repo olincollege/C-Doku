49935347df1cb7ceb2afb3ac35eb6763060ad965
allyson-hur <allysonhur@gmail.com>
2023-02-26 17:05:58 -0500

    Remove unnecessary functions and edit print_board_view()

--- src/view.c
+++ src/view.c
@@ -6,44 +6,31 @@
 
 #include <stdio.h>
 
-char board[9][9] = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
-// char board[9][9];
-void print_board_view()
+char player_board[9][9] = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
+
+void print_board_view(char board[9][9])
 {
     // prints the board which is a character array
-    printf("\n=====================================\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7], board[0][8]);
-    printf("\n|---:---:---|---:---:---|---:---:---|\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7], board[1][8]);
-    printf("\n|---:---:---|---:---:---|---:---:---|\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7], board[2][8]);
-    printf("\n=====================================\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7], board[3][8]);
-    printf("\n|---:---:---|---:---:---|---:---:---|\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[4][0], board[4][1], board[4][2], board[4][3], board[4][4], board[4][5], board[4][6], board[4][7], board[4][8]);
-    printf("\n|---:---:---|---:---:---|---:---:---|\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[5][0], board[5][1], board[5][2], board[5][3], board[5][4], board[5][5], board[5][6], board[5][7], board[5][8]);
-    printf("\n=====================================\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[6][0], board[6][1], board[6][2], board[6][3], board[6][4], board[6][5], board[6][6], board[6][7], board[6][8]);
-    printf("\n|---:---:---|---:---:---|---:---:---|\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[7][0], board[7][1], board[7][2], board[7][3], board[7][4], board[7][5], board[7][6], board[7][7], board[7][8]);
-    printf("\n|---:---:---|---:---:---|---:---:---|\n");
-    printf("| %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[8][0], board[8][1], board[8][2], board[8][3], board[8][4], board[8][5], board[7][6], board[7][7], board[7][8]);
-    printf("\n=====================================\n");
-}
-
-char convert_to_char(int n)
-{
-    char c = n + '0';
-    return c;
-}
-
-void update_board(int row, int col, int n)
-{
-    // take in parameters (row, col, #) and put it into the corresponding place on the board
-    // adding '0' to int n converts the int to a char
-    char c = convert_to_char(n);
-    board[row][col] = c;
+    printf("    1   2   3   4   5   6   7   8   9");
+    printf("\n  =====================================\n");
+    printf("1 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7], board[0][8]);
+    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
+    printf("2 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7], board[1][8]);
+    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
+    printf("3 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7], board[2][8]);
+    printf("\n  =====================================\n");
+    printf("4 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7], board[3][8]);
+    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
+    printf("5 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[4][0], board[4][1], board[4][2], board[4][3], board[4][4], board[4][5], board[4][6], board[4][7], board[4][8]);
+    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
+    printf("6 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[5][0], board[5][1], board[5][2], board[5][3], board[5][4], board[5][5], board[5][6], board[5][7], board[5][8]);
+    printf("\n  =====================================\n");
+    printf("7 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[6][0], board[6][1], board[6][2], board[6][3], board[6][4], board[6][5], board[6][6], board[6][7], board[6][8]);
+    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
+    printf("8 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[7][0], board[7][1], board[7][2], board[7][3], board[7][4], board[7][5], board[7][6], board[7][7], board[7][8]);
+    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
+    printf("9 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[8][0], board[8][1], board[8][2], board[8][3], board[8][4], board[8][5], board[7][6], board[7][7], board[7][8]);
+    printf("\n  =====================================\n");
 }
 
 void reset_board()
@@ -53,19 +40,19 @@ void reset_board()
     {
         for (int j = 0; j < 9; j++)
         {
-            board[i][j] = ' ';
+            player_board[i][j] = ' ';
         }
     }
 }
 int main(void)
 {
-    board[0][0] = '1';           // sets row 1, column 1 equal to 1
-    printf("%c\n", board[0][0]); // prints row 1, column 1
-    print_board_view();          // prints the board view
-    update_board(0, 0, 2);       // updates row 1, column 1 to be equal to 2
-    printf("%c\n", board[0][0]); // prints row 1, column 1
-    print_board_view();          // prints the updated board view
+    player_board[0][0] = '1';           // sets row 1, column 1 equal to 1
+    printf("%c\n", player_board[0][0]); // prints row 1, column 1
+    print_board_view(player_board);     // prints the board view
+    player_board[0][0] = '2';           // updates row 1, column 1 to be equal to 2
+    printf("%c\n", player_board[0][0]); // prints row 1, column 1
+    print_board_view(player_board);     // prints the updated board view
     reset_board();
-    printf("%c\n", board[0][1]); // prints row 1, column 1
-    print_board_view();
+    printf("%c\n", player_board[0][1]); // prints row 1, column 1
+    print_board_view(player_board);
 }
--- src/view.h
+++ src/view.h
@@ -8,33 +8,10 @@
  * Taking values from an existing array of arrays of characters, this function
  * prints a visualization of the player's sudoku board using a series of
  * formatted print statements.
+ *
+ * @param board The existing array of characters.
  */
-void print_board_view();
-
-/**
- * Convert an integer to a character.
- *
- * Given an integer, this function converts the integer to a character by adding
- * a null character to the integer.
- *
- * @param n The integer to be converted.
- * @return A char that represents the converted integer.
- */
-char convert_to_char(int n);
-
-/**
- * Change the value of a space on the board.
- *
- * Given a row, column, and number value (indicated by the user), this function
- * finds the corresponding spot on the visualized board (char array of arrays),
- * converts the number from an integer into a character, then inserts the
- * character into that spot on the board.
- *
- * @param row An integer representing the row on the board to edit.
- * @param col An integer representing the column on the board to edit.
- * @param n An integer representing the number value to print onto the board.
- */
-void update_board(int row, int col, int n);
+void print_board_view(char board[9][9]);
 
 /**
  * Reset the player's current sudoku board in terminal.
