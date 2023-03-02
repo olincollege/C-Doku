c25536ea47de0db0f5da696da4e6387ff472b991
Olga Pidruchna <olgapidruchna@gmail.com>
2023-03-01 23:21:46 -0500

    Fix documentation comments in model.h

--- src/model.c
+++ src/model.c
@@ -6,7 +6,6 @@ void update_board(var_game_state var) {
 }
 
 int check_fill(var_game_state var) {
-  // checks whether that spot is already filled
   if (var.player_board[var.p_move.row][var.p_move.col] != ' ') {
     return 1;
   }
@@ -14,17 +13,13 @@ int check_fill(var_game_state var) {
 }
 
 int move_correct(var_game_state var, const_game_state consts) {
-  // 0 = correct move
-  // 1 = incorrect move
   if (consts.solution_board[var.p_move.row][var.p_move.col] == var.p_move.num) {
     return 0;
   }
   return 1;
 }
 
-void board_complete(var_game_state var) {
-  // checks whether board is filled
-  // 0 = complete
+int board_complete(var_game_state var) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       if (var.player_board[i][j] == ' ') {
@@ -36,9 +31,6 @@ void board_complete(var_game_state var) {
 }
 
 int check_player_board(var_game_state var, const_game_state consts) {
-  // checks whether player board is filled in correctly compared to solution
-  // 1 means incorrect
-  // 0 means correct
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       if (var.player_board[i][j] != consts.solution_board[i][j]) {
@@ -50,7 +42,7 @@ int check_player_board(var_game_state var, const_game_state consts) {
 }
 
 int check_solution_board(void) {
+  // TO DO - Implement this function
   // checks whether the solution board actually follows all of the rules
-  // UPDATE THIS
   return 0;
 }
--- src/model.h
+++ src/model.h
@@ -6,62 +6,56 @@
 /**
  * Change the player's board according to the inputs given.
  *
- * Given the variable game state board, this function will replace the space in the player board provided by the user with the number provided by the user.
+ * Given the variable game state struct, this function will replace the space in
+ * the player board provided by the user with the number provided by the user.
  *
- * @param var a struct representing the variable game state
+ * @param var A struct that contains the variables in the game state.
  */
 void update_board(var_game_state var);
 
 /**
  * Check whether a spot is already filled.
  *
- * Given a row, column, and number from a player input, this function checks the
- * If the player's move is valid, the function returns 0. Otherwise, the
+ * Given the variable game state struct, this function checks whether the spot
+ * the user wants to input a number into is already filled.
+ * If the spot is empty, the function returns 0. Otherwise, the
  * function returns 1.
  *
- * @param board An array of arrays of integers that represent the player board.
- * @param row An integer representing the first array to index into.
- * @param col An integer representing the second array to index into.
- * @param num An integer representing the number to input into the board.
- * @return An integer representing the validity of the move (1 if invalid, 0 if
- * valid).
+ * @param var A struct that contains the variables in the game state.
+ * @return An integer representing whether the spot is filled (0 if
+ * empty, 1 if filled).
  */
 int check_fill(var_game_state var);
 
 /**
- * Check the validity of the move the player desires.
+ * Check whether the move is correct.
  *
- * Given a row, column, and number from a player input, this function checks the
- * following:
- *  - whether or not the spot is filled already
- *  - whether or not the number matches the answer key (is the right number)
- * If the player's move is valid, the function returns 0. Otherwise, the
+ * Given the variable game state struct and the constant game state struct, this
+ * function checks whether the player's move matches the solution board at that
+ * index. If the player's move is correct, the function returns 0. Otherwise, the
  * function returns 1.
  *
- * @param board An array of arrays of integers that represent the player board.
- * @param row An integer representing the first array to index into.
- * @param col An integer representing the second array to index into.
- * @param num An integer representing the number to input into the board.
- * @return An integer representing the validity of the move (1 if invalid, 0 if
- * valid).
+ * @param var A struct that contains the variables in the game state.
+ * @param consts A struct that contains the constants in the game state.
+ * @return An integer representing whether the move is correct (0 if
+ * correct, 1 if incorrect).
  */
 int move_correct(var_game_state var, const_game_state consts);
 
 /**
  * Check whether the board is completely filled or not.
  *
- * When called, checks the given board for whether each space is completely
- * filled. If so, the function change the value of a global variable from 0
- * (incomplete) to 1 (complete).
+ * When called, checks the given board for whether each space is
+ * filled. If so, the function returns 0. Otherwise, it returns 1.
  *
- * @param board An array of arrays of integers that represent the player board.
- * @param filled A global integer representing the state of the board (filled or
- * not).
+ * @param var A struct that contains the game state including the board.
+ * @return An integer representing whether the board is filled (0 if
+ * filled, 1 if not filled).
  */
-void board_complete(var_game_state var);
+int board_complete(var_game_state var);
 
 /**
- * Check whether the solution board is correct.
+ * Check whether the player's completed board is filled in correctly.
  *
  * When called, checks if the given board follows the game logic/rules. The
  * logic this function checks is as follows:
@@ -70,15 +64,17 @@ void board_complete(var_game_state var);
  *  - whether or not each in a 3x3 square of the 9x9 board contains different
  * numbers 1-9.
  *
- * Should the solution board be correctly filled, the function returns the
- * number 0. Otherwise, it returns the number 1.
+ * Should the solution board be correctly filled, the function returns 0.
+ * Otherwise, it returns 1.
  *
- * @param board An array of arrays of integers that represent the player board.
- * @return An integer representing the validity of the solution board.
+ * @param var A struct that contains the variables in the game state.
+ * @param consts A struct that contains the constants in the game state.
+ * @return An integer representing whether the board is correctly filled.
  */
 int check_player_board(var_game_state var, const_game_state consts);
 
 /**
+ * TO DO - Fix this after implementing this function
  * Check whether the solution board is correct.
  *
  * When called, checks if the given board follows the game logic/rules. The
@@ -91,7 +87,7 @@ int check_player_board(var_game_state var, const_game_state consts);
  * Should the solution board be correctly filled, the function returns the
  * number 0. Otherwise, it returns the number 1.
  *
- * @param board An array of arrays of integers that represent the player board.
+ * @param var A struct that contains the variables in the game state.
  * @return An integer representing the validity of the solution board.
  */
 int check_solution_board(void);
