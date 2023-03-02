fe320e37415b733418a60c70ace104ab79d075b0
allyson-hur <allysonhur@gmail.com>
2023-03-02 18:00:12 -0500

    Removed constants.h file

--- src/model.c
+++ src/model.c
@@ -1,29 +1,38 @@
 #include "model.h"
 #include <stdio.h>
 
-void update_board(var_game_state var) {
+void update_board(var_game_state var)
+{
   var.player_board[var.p_move.row][var.p_move.col] = var.p_move.num;
 }
 
-int check_fill(var_game_state var) {
-  if (var.player_board[var.p_move.row][var.p_move.col] != ' ') {
+int check_fill(var_game_state var)
+{
+  if (var.player_board[var.p_move.row][var.p_move.col] != ' ')
+  {
     return 1;
   }
   return 0;
 }
 
-int move_correct(var_game_state var, const_game_state consts) {
-  if (consts.solution_board[var.p_move.row][var.p_move.col] == var.p_move.num) {
+int move_correct(var_game_state var, const_game_state consts)
+{
+  if (consts.solution_board[var.p_move.row][var.p_move.col] == var.p_move.num)
+  {
     return 0;
   }
   return 1;
 }
 
 // TO DO Fix
-int board_complete(var_game_state var) {
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      if (var.player_board[i][j] == ' ') {
+int board_complete(var_game_state var)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      if (var.player_board[i][j] == ' ')
+      {
         return 1;
       }
     }
@@ -31,10 +40,14 @@ int board_complete(var_game_state var) {
   return 0;
 }
 
-int check_player_board(var_game_state var, const_game_state consts) {
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      if (var.player_board[i][j] != consts.solution_board[i][j]) {
+int check_player_board(var_game_state var, const_game_state consts)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      if (var.player_board[i][j] != consts.solution_board[i][j])
+      {
         return 1;
       }
     }
@@ -42,7 +55,8 @@ int check_player_board(var_game_state var, const_game_state consts) {
   return 0;
 }
 
-int check_solution_board(void) {
+int check_solution_board(void)
+{
   // TO DO - Implement this function
   // checks whether the solution board actually follows all of the rules
   return 0;
--- src/view.h
+++ src/view.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "model.h"
 
 /**
  * Print the player's current sudoku board in terminal.
@@ -7,7 +8,7 @@
  * prints a visualization of the player's sudoku board using a series of
  * formatted print statements.
  *
- * @param board The existing array of characters.
+ * @param board The existing char array of characters.
  */
 void print_board_view(char board[9][9]);
 
@@ -17,5 +18,7 @@ void print_board_view(char board[9][9]);
  * When the player is done playing and wants to play again, this function resets
  * the player board so the player can play again. This function iterates through
  * every cell of the player's sudoku board and sets it to an empty string.
+ *
+ * @param board The existing char array of characters.
  */
-void reset_board();
+void reset_board(char board[9][9]);
