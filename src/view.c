14ec04288058a72121cff38a074593b9bb02ff26
allyson-hur <allysonhur@gmail.com>
2023-03-08 10:42:30 -0500

    Created working test cases and added documentation

--- src/view.c
+++ src/view.c
@@ -1,47 +1,12 @@
-/**
- * Print out the visualization of the board in the terminal.
- *
- * This allows the user to update the board, then print the updated view of the board.
- */
-
 #include <stdio.h>
 #include <string.h>
 #include "view.h"
-#include "model.h"
-
-// char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-// char board[9][9] = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
-void print_board_view(char board[9][9])
-{
-    // prints the board which is a character array
-    printf("    1   2   3   4   5   6   7   8   9");
-    printf("\n  =====================================\n");
-    printf("1 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7], board[0][8]);
-    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
-    printf("2 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7], board[1][8]);
-    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
-    printf("3 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7], board[2][8]);
-    printf("\n  =====================================\n");
-    printf("4 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7], board[3][8]);
-    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
-    printf("5 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[4][0], board[4][1], board[4][2], board[4][3], board[4][4], board[4][5], board[4][6], board[4][7], board[4][8]);
-    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
-    printf("6 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[5][0], board[5][1], board[5][2], board[5][3], board[5][4], board[5][5], board[5][6], board[5][7], board[5][8]);
-    printf("\n  =====================================\n");
-    printf("7 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[6][0], board[6][1], board[6][2], board[6][3], board[6][4], board[6][5], board[6][6], board[6][7], board[6][8]);
-    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
-    printf("8 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[7][0], board[7][1], board[7][2], board[7][3], board[7][4], board[7][5], board[7][6], board[7][7], board[7][8]);
-    printf("\n  |---:---:---|---:---:---|---:---:---|\n");
-    printf("9 | %c : %c : %c | %c : %c : %c | %c : %c : %c |", board[8][0], board[8][1], board[8][2], board[8][3], board[8][4], board[8][5], board[7][6], board[7][7], board[7][8]);
-
-    printf("\n  =====================================\n");
-}
 
 void print_board(var_game_state *var)
 {
     const char *row_divider = "  |---:---:---|---:---:---|---:---:---|";
     const char *outer_border = "  =====================================";
-    printf("    1   2   3   4   5   6   7   8   9\n");
+    printf("\n    1   2   3   4   5   6   7   8   9\n");
 
     for (int row = 0; row < BOARD_DIM; ++row)
     {
@@ -71,43 +36,17 @@ void print_board(var_game_state *var)
     printf("%s\n", outer_border);
 }
 
-void reset_board(var_game_state *var)
-{
-    // clear board
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 9; j++)
-        {
-            var->player_board[i][j] = ' ';
-        }
-    }
-}
-
 int print_result(var_game_state *var)
 {
     if (var->errors == 3)
     {
-        printf("YOU LOSE :(");
+        printf("YOU LOSE :(\n");
         return 1;
     }
-    // if (board_complete(var) == 0)
-    // {
-    //     printf("YOU WIN! :D");
-    //     return 1;
-    // }
+    if (board_complete(var) == 0)
+    {
+        printf("YOU WIN! :D\n");
+        return 1;
+    }
     return 0;
 }
-// int main(void)
-// {
-//     var_game_state test, *var;
-//     char board[9][9] = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
-//     var = &test;
-//     // printf("%s\n", var);
-//     // printf("%c\n", var->player_board[0][5]);
-//     strcpy(var->player_board, board);
-//     print_board(var);
-//     //     player_board[5][7] = '2';
-//     //     player_board[1][3] = '9';
-//     //     print_board_view(player_board);
-//     // print_board_view(board);
-// }
--- test/test_view.c
+++ test/test_view.c
@@ -7,94 +7,167 @@
 #include "../src/model.h"
 
 // Check that an empty board prints correctly.
-Test(reset_board, empty_board, .init = cr_redirect_stdout)
+Test(print_board, empty_board, .init = cr_redirect_stdout)
 {
-    char player_board[9][9] = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
-    reset_board(player_board);
-    print_board_view(player_board);
+    var_game_state var;
+    for (size_t i = 0; i < BOARD_DIM; i++)
+    {
+        for (size_t j = 0; j < BOARD_DIM; j++)
+        {
+            var.player_board[i][j] = ' ';
+        }
+    }
+    print_board(&var);
     (void)fflush(stdout);
     (void)fclose(stdout);
     cr_assert_stdout_eq_str(
-        "\n    1   2   3   4   5   6   7   8   9"
-        "\n  =====================================\n"
-        "1 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "2 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "3 |   :   :   |   :   :   |   :   :   |"
-        "\n  =====================================\n"
-        "4 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "5 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "6 |   :   :   |   :   :   |   :   :   |"
-        "\n  =====================================\n"
-        "7 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "8 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "9 |   :   :   |   :   :   |   :   :   |"
-        "\n  =====================================\n");
+        "\n    1   2   3   4   5   6   7   8   9\n"
+        "  =====================================\n"
+        "1 |   :   :   |   :   :   |   :   :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "2 |   :   :   |   :   :   |   :   :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "3 |   :   :   |   :   :   |   :   :   |\n"
+        "  =====================================\n"
+        "4 |   :   :   |   :   :   |   :   :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "5 |   :   :   |   :   :   |   :   :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "6 |   :   :   |   :   :   |   :   :   |\n"
+        "  =====================================\n"
+        "7 |   :   :   |   :   :   |   :   :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "8 |   :   :   |   :   :   |   :   :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "9 |   :   :   |   :   :   |   :   :   |\n"
+        "  =====================================\n");
 }
-// Test that a cell prints correctly
 
-Test(print_board_view, filled_cells, .init = cr_redirect_stdout)
+// Check that a partially filled board prints correctly
+Test(print_board, partially_filled_board, .init = cr_redirect_stdout)
 {
-    char player_board[9][9] = {{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
-    int row_number1 = 2;
-    int column_number1 = 4;
-    int row_number2 = 6;
-    int column_number2 = 8;
-
-    // subtract 1 from integer variables to account for index starting at 0
-    player_board[row_number1 - 1][column_number1 - 1] = '9';
-    player_board[row_number2 - 1][column_number2 - 1] = '2';
-    print_board_view(player_board);
+    var_game_state var;
+    const char board[BOARD_DIM][BOARD_DIM] = {
+        " 9 28   6",
+        "62    1 8",
+        "  7    3 ",
+        "8 31  4  ",
+        "    7 36 ",
+        " 79      ",
+        "1    49 2",
+        "94    87 ",
+        "     8   "};
+    for (size_t i = 0; i < BOARD_DIM; i++)
+    {
+        for (size_t j = 0; j < BOARD_DIM; j++)
+        {
+            var.player_board[i][j] = board[i][j];
+        }
+    }
+    print_board(&var);
     (void)fflush(stdout);
     (void)fclose(stdout);
     cr_assert_stdout_eq_str(
-        "\n    1   2   3   4   5   6   7   8   9"
-        "\n  =====================================\n"
-        "1 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "2 |   :   : 9 |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "3 |   :   :   |   :   :   |   :   :   |"
-        "\n  =====================================\n"
-        "4 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "5 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "6 |   :   :   |   :   :   |   : 2 :   |"
-        "\n  =====================================\n"
-        "7 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "8 |   :   :   |   :   :   |   :   :   |"
-        "\n  |---:---:---|---:---:---|---:---:---|\n"
-        "9 |   :   :   |   :   :   |   :   :   |"
-        "\n  =====================================\n");
+        "\n    1   2   3   4   5   6   7   8   9\n"
+        "  =====================================\n"
+        "1 |   : 9 :   | 2 : 8 :   |   :   : 6 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "2 | 6 : 2 :   |   :   :   | 1 :   : 8 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "3 |   :   : 7 |   :   :   |   : 3 :   |\n"
+        "  =====================================\n"
+        "4 | 8 :   : 3 | 1 :   :   | 4 :   :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "5 |   :   :   |   : 7 :   | 3 : 6 :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "6 |   : 7 : 9 |   :   :   |   :   :   |\n"
+        "  =====================================\n"
+        "7 | 1 :   :   |   :   : 4 | 9 :   : 2 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "8 | 9 : 4 :   |   :   :   | 8 : 7 :   |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "9 |   :   :   |   :   : 8 |   :   :   |\n"
+        "  =====================================\n");
 }
 
+// Check that a complete board prints correctly
+Test(print_board, filled_cells, .init = cr_redirect_stdout)
+{
+    var_game_state var;
+    const char board[BOARD_DIM][BOARD_DIM] = {"391285746", "625437198", "487916235",
+                                              "863159427", "514872369", "279643581",
+                                              "138764952", "946521873", "752398614"};
+
+    for (size_t i = 0; i < BOARD_DIM; i++)
+    {
+        for (size_t j = 0; j < BOARD_DIM; j++)
+        {
+            var.player_board[i][j] = board[i][j];
+        }
+    }
+    print_board(&var);
+    (void)fflush(stdout);
+    (void)fclose(stdout);
+    cr_assert_stdout_eq_str(
+        "\n    1   2   3   4   5   6   7   8   9\n"
+        "  =====================================\n"
+        "1 | 3 : 9 : 1 | 2 : 8 : 5 | 7 : 4 : 6 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "2 | 6 : 2 : 5 | 4 : 3 : 7 | 1 : 9 : 8 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "3 | 4 : 8 : 7 | 9 : 1 : 6 | 2 : 3 : 5 |\n"
+        "  =====================================\n"
+        "4 | 8 : 6 : 3 | 1 : 5 : 9 | 4 : 2 : 7 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "5 | 5 : 1 : 4 | 8 : 7 : 2 | 3 : 6 : 9 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "6 | 2 : 7 : 9 | 6 : 4 : 3 | 5 : 8 : 1 |\n"
+        "  =====================================\n"
+        "7 | 1 : 3 : 8 | 7 : 6 : 4 | 9 : 5 : 2 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "8 | 9 : 4 : 6 | 5 : 2 : 1 | 8 : 7 : 3 |\n"
+        "  |---:---:---|---:---:---|---:---:---|\n"
+        "9 | 7 : 5 : 2 | 3 : 9 : 8 | 6 : 1 : 4 |\n"
+        "  =====================================\n");
+}
+
+// Check that the game result is a loss when the number of errors equals 3
 Test(print_result, game_loss, .init = cr_redirect_stdout)
 {
-    var_game_state *var;
-    var->errors == 3;
-    print_result(var);
+    var_game_state var;
+    var.errors = 3;
+    print_result(&var);
     (void)fflush(stdout);
     (void)fclose(stdout);
-    cr_assert_stdout_eq_str(
-        "YOU LOSE :(");
-    cr_assert(eq(int, print_result(var), 1));
+    cr_expect(eq(int, print_result(&var), 1));
+    cr_expect_stdout_eq_str(
+        "YOU LOSE :(\n");
 }
 
+// Check that the game result is a win when the number of errors is less than 3
+// and the board is complete
 Test(print_result, game_win, .init = cr_redirect_stdout)
 {
-    var_game_state *var = 2;
-    board_complete(var) == 0;
-    print_result(var);
+    var_game_state var;
+    var.errors = 2;
+    board_complete(&var) == 0;
+    print_result(&var);
     (void)fflush(stdout);
     (void)fclose(stdout);
-    cr_assert_stdout_eq_str(
-        "YOU WIN! :D");
-    cr_assert(eq(int, print_result(var), 1));
+    cr_expect_stdout_eq_str(
+        "YOU WIN! :D\n");
+    cr_expect(eq(int, print_result(&var), 1));
+}
+
+// Check that the game continues when the number of errors is less than 3
+// and the board is incomplete
+Test(print_result, game_continue, .init = cr_redirect_stdout)
+{
+    var_game_state var;
+    var.errors = 1;
+    board_complete(&var) == 0;
+    print_result(&var);
+    (void)fflush(stdout);
+    (void)fclose(stdout);
+    cr_assert(eq(int, print_result(&var), 0));
 }
