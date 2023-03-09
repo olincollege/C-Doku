7bf2c15e8a8f0d7db2d1da304d4617dfd97f2101
Olga Pidruchna <olgapidruchna@gmail.com>
2023-03-08 23:13:24 -0500

    Debug main to handle inputs correctly

--- src/controller.h
+++ src/controller.h
@@ -16,10 +16,9 @@
  * indexes through the string and checks that characters in each slot of the array are
  * of the correct type they are supposed to be in. For example, characters in index slots
  * that are even should be integers, while the odd-numbered index slots should be commas.
- * Should the input be of the wrong type, the function returns 1. However, if the input
- * was correct, the function returns 0.
+ * If the input is in the correct format, the function returns 0, and it otherwise returns 1.
  *
- * @param input An array of characters representing the original string to verify.
+ * @param input A string representing the original string to verify.
  * @return An integer indicating the input is correct (0) or not (1).
  */
 int check_valid_input(char *input);
@@ -27,11 +26,14 @@ int check_valid_input(char *input);
 /**
  * Asks player for a move and saves the raw input.
  *
- * When called, this function uses printf() to ask the player for a board input,
- * and displays the correct format to input their move. The fgets() function is then
- * called to save the raw input from the player's keyboard.
+ * When called, this function uses printf() to ask the player for their move,
+ * and displays the correct format to input it. The fgets() function is then
+ * called to save the raw input from the player's keyboard into a given string.
+ * If the player's move is too long or not successfully obtained, the function
+ * will return 1, or 0 otherwise.
  *
- * @return A string representing the raw input from the player.
+ * @param input A string that will hold the move that the player inputs.
+ * @return An integer representing whether 
  */
 int get_player_input(char *input);
 
@@ -42,8 +44,8 @@ int get_player_input(char *input);
  * into an array. This function assumes that the first-appearing integer is the row, the
  * second-appearing integer is the column, and the third-appearing integer is the number.
  *
- * @param move An array of characters representing the player's raw input to parse.
- * @return An array of integers representing the row, column, and number of the player's
- * move, in that order.
+ * @param input An array of characters representing the player's raw input to parse.
+ * @param var 
+ * @return An integer representing whether the function ran succesfully (0) or not (1).
  */
 int add_player_move(char *input, var_game_state *var);
--- src/view.h
+++ src/view.h
@@ -2,25 +2,27 @@
 #include "model.h"
 
 /**
- * Print the visualization of the player's sudoku board.
+ * Prints the visualization of the player's sudoku board.
  *
  * Given the variable game state struct, this function prints a visualization
  * of the player's sudoku board using a series of formatted print statements.
  *
- * @param var A struct that contains the variables in the game state.
+ * @param var A pointer to a struct that contains the variables in the game state.
  */
 void print_board(var_game_state *var);
 
 /**
- * Print the result of the game.
+ * Prints the result of the game.
  *
- * When the player gets exactly 3 errors before the board is filled, then the
- * functions prints out that the player lost and the game ends. If the player is
- * able to fill the board before the error reaches 3, then the function prints that
- * the player won and the game ends. Otherwise, the game continues.
+ * If the player gets exactly 3 errors before the board has been filled, then
+ * the function prints out that the player lost and the function returns 1,
+ * indicating a game end condition. If the player has filled the board before
+ * the error counter reaches 3, then the function prints that the player won and
+ * returns 1. Otherwise, the function returns 0, indicating that the game
+ * continues.
  *
- * @param var A struct that contains the variables in the game state.
- * @return An integer representing if the game has finished (1 if the player either wins or loses,
+ * @param var A pointer to a struct that contains the variables in the game state.
+ * @return An integer representing whether the game has finished (1 if the player either wins or loses,
  *  and 0 otherwise).
  */
 int print_result(var_game_state *var);
