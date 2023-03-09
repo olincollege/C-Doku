05ba43b64f2537f7bfcca00274875d265b7dd385
allyson-hur <allysonhur@gmail.com>
2023-03-08 23:22:33 -0500

    Removed build file

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/allysonhur/C-Doku
-# Build directory: /home/allysonhur/C-Doku/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
