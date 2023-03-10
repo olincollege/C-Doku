7b169411d7062658e746078009fdf34147dea521
Olga Pidruchna <olgapidruchna@gmail.com>
2023-03-09 22:39:07 -0500

    Fix documentation in test model

--- test/test_model.c
+++ test/test_model.c
@@ -32,128 +32,125 @@ const_game_state make_const_complete_board(void) {
   return solution_board;
 }
 
+// A correct move should be added to the first spot (upper left corner) of the board.
 Test(update_board, correct_move_1) {
   var_game_state test = make_starting_board();
   test.p_move = (move){0, 0, '3'};
   update_board(&test);
   cr_assert(eq(chr, test.player_board[test.p_move.row][test.p_move.col], '3'));
 }
-// Update [9][9] = 4
+
+// A correct move should be added to the last spot (lower right corner) of the board.
 Test(update_board, correct_move_2) {
   var_game_state test = make_starting_board();
   test.p_move = (move){9, 9, '4'};
   update_board(&test);
   cr_assert(eq(chr, test.player_board[test.p_move.row][test.p_move.col], '4'));
 }
-// Update [5][3] = 6
+
+// A correct move should be added to any spot in the middle of the board.
 Test(update_board, correct_move_3) {
   var_game_state test = make_starting_board();
   test.p_move = (move){5, 3, '6'};
   update_board(&test);
   cr_assert(eq(chr, test.player_board[test.p_move.row][test.p_move.col], '6'));
 }
-// Wrong Move: value incorrect from board complete
-// Update [0][2] with 4, should be 1
+
+// A move should be added to the board even if it is wrong.
 Test(update_board, wrong_move_overwrite_1) {
   var_game_state test = make_starting_board();
   test.p_move = (move){0, 2, '4'};
   update_board(&test);
   cr_assert(eq(chr, test.player_board[test.p_move.row][test.p_move.col], '4'));
 }
-// Wrong Move: overwrite a filled board with wrong number
-// Overwrite [0][1] = 9 with 1
+
+// A move should be added to the board even if it overwrites another number.
 Test(update_board, wrong_move_overwrite_2) {
   var_game_state test = make_starting_board();
   test.p_move = (move){0, 1, '1'};
   update_board(&test);
   cr_assert(eq(chr, test.player_board[test.p_move.row][test.p_move.col], '1'));
 }
-// Wrong Move: overwrite a filled board with wrong number
-// Overwrite [6][5] = 4 with 8
-Test(update_board, wrong_move_overwrite_3) {
-  var_game_state test = make_starting_board();
-  test.p_move = (move){6, 5, '8'};
-  update_board(&test);
-  cr_assert(eq(chr, test.player_board[test.p_move.row][test.p_move.col], '8'));
-}
 
-// check_fill: 2 filled case, 1 empty case
-// Check a filled spot
+// A filled spot should report filled.
 Test(check_filled, filled_1) {
   var_game_state test = make_starting_board();
   test.p_move = (move){0, 1, '2'};
   cr_assert(eq(int, check_fill(&test), 1));
 }
-Test(check_filled, filled_2) {
-  var_game_state test = make_complete_board();
-  test.p_move = (move){5, 8, '2'};
-  cr_assert(eq(int, check_fill(&test), 1));
-}
-// Check a empty spot
+
+// An empty spot should report empty.
 Test(check_filled, empty_1) {
   var_game_state test = make_starting_board();
   test.p_move = (move){0, 0, '3'};
   cr_assert(eq(int, check_fill(&test), 0));
 }
 
-// check_move
-// Move correct
+// A correct move should report correct.
 Test(move_correct, move_correct_1) {
   var_game_state test = make_starting_board();
   const_game_state answer = make_const_complete_board();
   test.p_move = (move){0, 0, '3'};
   cr_assert(eq(int, move_correct(&test, &answer), 0));
 }
+
+// An incorrect move should report incorrect.
 Test(move_correct, move_incorrect_1) {
   var_game_state test = make_starting_board();
   const_game_state answer = make_const_complete_board();
   test.p_move = (move){0, 0, 4};
   cr_assert(eq(int, move_correct(&test, &answer), 1));
 }
-// board_complete
-// complete
+
+// A complete board should report complete.
 Test(board_complete, complete_1) {
   var_game_state test = make_complete_board();
   cr_assert(eq(int, board_complete(&test), 0));
 }
-// incomplete starting board
+
+// An intial board should report incomplete.
 Test(board_complete, incomplete_start) {
   var_game_state test = make_starting_board();
   cr_assert(eq(int, board_complete(&test), 1));
 }
-// incomplete complete board with last value empty
+
+// An almost complete board with one empty spot should report incomplete.
 Test(board_complete, incomplete_1_less) {
   var_game_state test = make_complete_board();
   test.player_board[8][8] = ' ';
   cr_assert(eq(int, board_complete(&test), 1));
 }
-// check_player_board
-// Correct
+
+// A completely filled board should report correctly filled.
 Test(check_player_board, correct_1) {
   var_game_state test = make_complete_board();
   const_game_state answer = make_const_complete_board();
   cr_assert(eq(int, check_player_board(&test, &answer), 0));
 }
-// Incorrect: start board vs complete
+
+// An initial board should report not correctly filled.
 Test(check_player_board, incorrect_start) {
   var_game_state test = make_starting_board();
   const_game_state answer = make_const_complete_board();
   cr_assert(eq(int, check_player_board(&test, &answer), 1));
 }
-// Incorrect: complete board with 1 empty spot vs complete
+// A board that's almost complete with one empty spot should report not correctly filled.
 Test(check_player_board, incorrect_missing) {
   var_game_state test = make_complete_board();
   test.player_board[8][8] = ' ';
   const_game_state answer = make_const_complete_board();
   cr_assert(eq(int, check_player_board(&test, &answer), 1));
 }
-// Incorrect: complete board with 1 wrong number vs complete
+
+// A board with one incorrect number should report not correctly filled.
 Test(check_player_board, incorrect_1_number) {
   var_game_state test = make_complete_board();
   test.player_board[8][8] = '5';
   const_game_state answer = make_const_complete_board();
   cr_assert(eq(int, check_player_board(&test, &answer), 1));
 }
+
+// check whether the boards that we entered manually follow sudoku rules
 
 // check_solution_board
 Test(check_solution_board, correct_1) {
@@ -244,12 +241,14 @@ Test(check_solution_board, correct_hard_2) {
   cr_assert(eq(int, check_solution_board(&answer), 0));
 }
 
-// incorrect_board
+// Solution board with one incorrect value should report incorrect.
 Test(check_solution_board, incorrect_1) {
   const_game_state answer = make_const_complete_board();
   answer.solution_board[8][8] = '5';
   cr_assert(eq(int, check_solution_board(&answer), 1));
 }
+
+// Solution board with letters instead of numbers should report incorrect.
 Test(check_solution_board, incorrect_characters) {
   const_game_state answer = {
       .solution_board = {"abcdefghi", "abcdefghi", "abcdefghi", "abcdefghi",
@@ -258,6 +257,8 @@ Test(check_solution_board, incorrect_characters) {
   };
   cr_assert(eq(int, check_solution_board(&answer), 1));
 }
+
+// Solution board with numbers out of range (less than 1 or greater than 9) should report incorrect.
 Test(check_solution_board, incorrect_range) {
   const_game_state answer = {
       .solution_board = {"390285746", "625437098", "487906235", "863059427",
@@ -265,4 +266,6 @@ Test(check_solution_board, incorrect_range) {
                          "752398604"}};
   cr_assert(eq(int, check_solution_board(&answer), 1));
 }
+
+
 // NOLINTEND(*-magic-numbers)
